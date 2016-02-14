4138d27122e27d8485e4347c108140ebc50e0677
Mohammad Pezeshki <mohammadpz@gmail.com>
2016-02-14 17:57:01 -0500

    grad test adde

--- examples/rnnctc.py
+++ examples/rnnctc.py
@@ -38,29 +38,20 @@ network_output = lasagne.layers.get_output(l_out)
 
 cost = T.mean(ctc.cpu_ctc_th(network_output, input_lens, output, output_lens))
 grads = T.grad(cost, wrt=network_output)
+gradsx2 = T.grad(cost * 2, wrt=network_output)
 all_params = lasagne.layers.get_all_params(l_out)
 updates = lasagne.updates.adam(cost, all_params, 0.001)
 
 train = theano.function([l_in.input_var, input_lens, output, output_lens], cost, updates=updates)
 predict = theano.function([l_in.input_var], network_output)
-get_grad = theano.function([l_in.input_var, input_lens, output, output_lens], grads)
+get_grad = theano.function([l_in.input_var, input_lens, output, output_lens],
+                           [grads, gradsx2],
+                           allow_input_downcast=True)
 
 from loader import DataLoader
 data_loader = DataLoader(mbsz=mbsz, min_len=min_len, max_len=max_len, num_classes=num_classes)
 
-i = 1
-while True:
-    i += 1
-    print i
-    sample = data_loader.sample()
-    cost = train(*sample)
-    out = predict(sample[0])
-    print cost
-    print "input", sample[0][0].argmax(1)
-    print "prediction", out[:, 0].argmax(1)
-    print "expected", sample[2][:sample[3][0]]
-    if i == 10000:
-        grads = get_grad(*sample)
-        import ipdb; ipdb.set_trace()
-
+sample = data_loader.sample()
+print np.mean(abs(get_grad(*sample)[0]))
+print np.mean(abs(get_grad(*sample)[1]))
 
